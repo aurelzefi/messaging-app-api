a4244d21a9f177b1646fd8f6067bdc267ef4a823
Aurel Zefi <aurelzefi1994@gmail.com>
2020-08-01 14:24:02 +0200

    wip

---
+++ app/Http/Middleware/AuthenticateWithQueryParameter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AuthenticateWithQueryParameter
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $token = $request->input('api_token');
+
+        if ($token) {
+            $request->headers->set('Authorization', "Bearer $token");
+        }
+
+        return $next($request);
+    }
+}
