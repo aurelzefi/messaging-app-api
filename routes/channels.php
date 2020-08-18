8f4c43f7676299df50630fa4563cf3529da9b4fd
Aurel Zefi <aurelzefi1994@gmail.com>
2020-08-18 21:52:07 +0200

    wip

--- routes/channels.php
+++ routes/channels.php
@@ -17,6 +17,10 @@ Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
+Broadcast::channel('online', function ($user) {
+    return $user;
+});
+
 Broadcast::channel('typing.{id}', function () {
     return true;
 });
